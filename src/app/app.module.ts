54dc6782cfd74a2a8472c3a9745de3eefc36b1d9
Belchenkov <u608110@gmail.com>
2017-07-20 11:45:46 +0300

    AngularFire Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule,  Routes } from '@angular/router';
 
+// AngularFire Imports
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+// Component Imports
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientsComponent } from './components/clients/clients.component';
@@ -15,11 +21,22 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
+// Service Imports
+import { ClientService } from './services/client.service';
+
 const appRoutes: Routes = [
   {path: '', component: DashboardComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent}
 ];
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyC1ehTbcLD9dEHDlk4g26ztpbd05Unj0es",
+    authDomain: "clientpanel-7c445.firebaseapp.com",
+    databaseURL: "https://clientpanel-7c445.firebaseio.com",
+    storageBucket: "clientpanel-7c445.appspot.com",
+    messagingSenderId: "864017577930"
+};
 
 @NgModule({
   declarations: [
@@ -38,9 +55,16 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    ClientService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
